31573b388fcf9804e3eaa2da6b5e4df6fe4a0618
Xiao Liu <liuxiao.qmbupt@gmail.com>
2019-03-19 22:56:34 -0700

    Hello World --- Django Template

---
+++ eCommerce/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/xiaoliu/Courses/CodingForEntrepreneurs/newStart/eCommerce/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
