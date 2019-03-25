53ab4af159d5bbe214fdf1a920cff7eac6eae2ad
Xiao Liu <liuxiao.qmbupt@gmail.com>
2019-03-24 23:04:43 -0700

    Hello World --- Django forms

--- eCommerce/src/ecommerce/views.py
+++ eCommerce/src/ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
 	context = {
 		"title":"Hello World!",
@@ -16,15 +18,19 @@ def about_page(request):
 	return render(request, "home_page.html", context)
 
 def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
 	context = {
 		"title":"about",
-		"content":"Welcome to the contactpage"
+		"content":"Welcome to the contactpage",
+		"form": contact_form
 	}
-	if request.method == "POST":
-		print(request.POST)
-		print(request.POST.get('fullname'))
-		print(request.POST.get('email'))
-		print(request.POST.get('content'))
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	# if request.method == "POST":
+	# 	#print(request.POST)
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('email'))
+	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
 
 def home_page_old(request):
