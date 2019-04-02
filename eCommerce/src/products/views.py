f68d86803561c21c8dd911ce1b0404a90ace89f9
Xiao Liu <liuxiao.qmbupt@gmail.com>
2019-04-01 23:28:33 -0700

    Add detail view for product model

--- eCommerce/src/products/views.py
+++ eCommerce/src/products/views.py
@@ -1,10 +1,31 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 # Create your views here.
 
 # Class based view
+class ProductDetailView(DetailView):
+	queryset = Product.objects.all()
+	template_name = "products/detail.html"
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+		print(context)
+		return context
+
+
+# Function based view
+def product_detail_view(request, pk=None, *args, **kwargs):
+	#instance = Product.objects.get(pk=pk)
+	instance = get_object_or_404(Product, pk=pk)
+	context = {
+		'object_list': instance
+	}
+	return render(request, "products/detail.html", context)
+
+
+# Class based viewf/
 class ProductListView(ListView):
 	queryset = Product.objects.all()
 	template_name = "products/list.html"
@@ -14,8 +35,6 @@ class ProductListView(ListView):
 	# 	print(context)
 	# 	return context
 
-
-# Function based view
 def product_list_view(request):
 	queryset = Product.objects.all()
 	context = {
